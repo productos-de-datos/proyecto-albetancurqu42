434cb5779a0b1bd75a0a16a7462e43e889d23f54
albetancurqu <albetancurqu@unal.edu.co>
2022-07-02 14:21:40 -0500

    Se agrega transformacion de datos

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -1,3 +1,14 @@
+import pathlib
+
+import pandas as pd
+
+import utils
+
+base_path = pathlib.Path.cwd()
+landing_path = base_path.joinpath('data_lake/landing')
+raw_path = base_path.joinpath('data_lake/raw')
+
+
 def transform_data():
     """Transforme los archivos xls a csv.
 
@@ -7,10 +18,37 @@ def transform_data():
     H23.
 
     """
-    raise NotImplementedError("Implementar esta función")
+    list_excel_files = sorted(list(landing_path.glob('*.xl*')))
+
+    for excel_file in list_excel_files:
+        df_raw = pd.read_excel(excel_file, index_col=None, header=None)
+
+        df_where_value = df_raw.eq('Fecha')
+        pos_index_value = df_where_value.loc[df_where_value.any(axis=1), :].index.values[0]
+        pos_col_value = df_where_value.loc[:, df_where_value.any(axis=0)].columns.values[0]
+
+        df_clean = df_raw.copy()
+        df_clean = df_clean.loc[pos_index_value:, pos_col_value:]
+        df_clean = df_clean.set_index(0)
+        df_clean = df_clean.drop(index='Fecha')
+
+        if len(df_clean.columns) > 24:
+            df_clean = df_clean.iloc[:, :24]
+
+        no_date_index = pd.to_datetime(df_clean.index, errors='coerce')
+        df_clean = df_clean.loc[~no_date_index.isna()]
+
+        df_clean.index = pd.to_datetime(df_clean.index)
+        df_clean = df_clean[~df_clean.index.duplicated(keep='first')]
+
+        df_clean.columns = [f'H{hour:02d}' for hour in range(0, 24)]
+        df_clean.index.name = None
+
+        df_clean.to_csv(raw_path.joinpath(f'{excel_file.stem}.csv'))
 
 
 if __name__ == "__main__":
     import doctest
 
+    transform_data()
     doctest.testmod()
