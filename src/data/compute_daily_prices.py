8dada5558f47ba4e7d668321d672d7b22b72db90
albetancurqu <albetancurqu@unal.edu.co>
2022-07-04 17:50:12 -0500

    Se agregan test datos mensuales y diarios

--- src/data/compute_daily_prices.py
+++ src/data/compute_daily_prices.py
@@ -7,7 +7,12 @@ cleansed_path = base_path.joinpath("data_lake/cleansed")
 business_path = base_path.joinpath("data_lake/business")
 
 
-def compute_daily_prices(source_path=cleansed_path, target_path=business_path):
+def compute_daily_prices(
+    source_path=cleansed_path,
+    target_path=business_path,
+    source_filename="precios-horarios.csv",
+    target_namefile="precios-diarios.csv",
+):
     """Compute los precios promedios diarios.
 
     Usando el archivo data_lake/cleansed/precios-horarios.csv, compute el precio
@@ -19,11 +24,11 @@ def compute_daily_prices(source_path=cleansed_path, target_path=business_path):
 
     """
 
-    df_hourly_prices = read_format_hourly_prices(source_path)
+    df_hourly_prices = read_format_hourly_prices(source_path, filename=source_filename)
 
     df_daily_prices = resample_hourly_prices(df_hourly_prices, freq="D")
 
-    df_daily_prices.to_csv(target_path.joinpath("precios-diarios.csv"))
+    df_daily_prices.to_csv(target_path.joinpath(target_namefile))
 
 
 if __name__ == "__main__":
--- src/data/compute_monthly_prices.py
+++ src/data/compute_monthly_prices.py
@@ -7,7 +7,12 @@ cleansed_path = base_path.joinpath("data_lake/cleansed")
 business_path = base_path.joinpath("data_lake/business")
 
 
-def compute_monthly_prices(source_path=cleansed_path, target_path=business_path):
+def compute_monthly_prices(
+    source_path=cleansed_path,
+    target_path=business_path,
+    source_filename="precios-horarios.csv",
+    target_namefile="precios-mensuales.csv",
+):
     """Compute los precios promedios mensuales.
 
     Usando el archivo data_lake/cleansed/precios-horarios.csv, compute el prcio
@@ -17,11 +22,11 @@ def compute_monthly_prices(source_path=cleansed_path, target_path=business_path)
     * fecha: fecha en formato YYYY-MM-DD
     * precio: precio promedio mensual de la electricidad en la bolsa nacional
     """
-    df_hourly_prices = read_format_hourly_prices(source_path)
+    df_hourly_prices = read_format_hourly_prices(source_path, filename=source_filename)
 
     df_monthly_prices = resample_hourly_prices(df_hourly_prices, freq="MS")
 
-    df_monthly_prices.to_csv(target_path.joinpath("precios-mensuales.csv"))
+    df_monthly_prices.to_csv(target_path.joinpath(target_namefile))
 
 
 if __name__ == "__main__":
